b6e713449f3c195420ee6e1464514eb3c5e470df
Harper Reed <harper@nata2.org>
2012-10-23 10:32:38 -0500

    new settings file

--- build_settings_yaml.go
+++ build_settings_yaml.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"fmt"
+	"github.com/gosexy/sugar"
 	"github.com/gosexy/yaml"
 	"os"
 )
 
 func main() {
 
-	settings_filename := "settings.yaml"
+	settings_filename := "settingss.yaml"
 
 	if _, err := os.Stat(settings_filename); err != nil {
 		if os.IsNotExist(err) {
@@ -20,6 +21,8 @@ func main() {
 			settings.Set("connection/realname", "Go Bot")
 
 			settings.Set("bot_config/rejoin_on_kick", true)
+			settings.Set("bot_config/owner", "example!example@example/example")
+			settings.Set("bot_config/friends", sugar.List{"friend1!example@example/example", "friend2!example@example/example", "friend2!example@example/example"})
 			settings.Write(settings_filename)
 		} else {
 
